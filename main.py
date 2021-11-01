646918a762e76a83a435502f466be677e703cdaa
larissa.justen <larissa.justen@cwi.com.br>
2021-11-01 00:48:08 -0300

    Remove extra comments

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-"""
-JOGO DA FORCA
-Coisas a implementar:
-- Array com palavras a serem escolhidas quando um jogo começar
-- Função para gerar um número inteiro aleatório de 0 até comprimento do Array de palavras
-- Função para printar no console a linha mostrando quantidade de letras na palavra, as letras já acertadas e vidas restantes
-- Lógica de restart de jogo
-"""
 import database
 import random
 import game
@@ -15,23 +7,17 @@ def _choose_random_word():
     index_random_word = random.randint(0, num_words-1)
     return database.words[index_random_word]
 
-while True:  # Loop para iniciar o jogo
-    while True:  # Loop para pegar a resposta do usuário
+while True:
+    while True:
         start = input('Você deseja iniciar um jogo de forca? [S/N]').upper().strip()
 
         if start == 'N' or start == 'S':
             break
         print('Coloque "S" para comecar o jogo ou "N" para sair do jogo')
 
-    if start == 'N':  # Finaliza o jogo caso o usuário não queira jogar
+    if start == 'N':
         print('Fechando o jogo...')
         break
 
-    # Busca uma palavra aleatória
     word = _choose_random_word()
     game.play(word.upper())
-
-    # "Criptografar" e mostrar a palavra para o usuário
-    # Manter um array com as letras já escolhidas para o usuário e ir mostrando gradativamente ao usuário
-    # Manter um controle da quantidade de vidas e tentativas do usuário
-    # Determinar se ganhou ou não
