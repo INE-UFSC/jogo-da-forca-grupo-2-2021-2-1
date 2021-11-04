6a2b0290c6e8fd68d020c6629bf0a73cb38e4bab
Rafael Vargas <rafael.s.vargas18@gmail.com>
2021-11-04 10:12:46 -0400

    Adding new word to database and changing first question

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import database
 import random
 import game
 
+
 def _choose_random_word():
     num_words = len(database.words)
     index_random_word = random.randint(0, num_words-1)
     return database.words[index_random_word]
 
+
 while True:
     while True:
-        start = input('Você deseja iniciar um jogo de forca? [S/N]').upper().strip()
+        start = input(
+            'Você deseja iniciar um jogo de forca? [S/N]: ').upper().strip()
 
         if start == 'N' or start == 'S':
             break
